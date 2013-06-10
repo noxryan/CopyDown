767015f68fc5614bbc0fe3be54dbe1834be9ab06
Ryan Schlesinger <noxryan@gmail.com>
2013-06-10 16:30:21 -0500

    init
    Signed-off-by: Ryan Schlesinger <noxryan@gmail.com>

---
+++ CopyDown/Guids.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+
+Class GuidList
+    Private Sub New()
+    End Sub
+
+    Public Const guidCopyDownPkgString As String = "cd470a91-4513-4453-9b81-62dcf22c6e2c"
+    Public Const guidCopyDownCmdSetString As String = "cbd6f49b-ef27-4253-ab44-12e8254a4064"
+
+    Public Shared ReadOnly guidCopyDownCmdSet As New Guid(guidCopyDownCmdSetString)
+End Class
---
+++ CopyDown/PkgCmdID.vb
@@ -0,0 +1,11 @@
+﻿' PkgCmdID.vb
+Imports System
+
+Class PkgCmdIDList
+    Private Sub New()
+    End Sub
+
+        Public Const CopyDown As UInteger = &H100
+
+
+End Class
